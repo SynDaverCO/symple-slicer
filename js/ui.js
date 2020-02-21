65da4448baeac508687b6b8d2bae874393dc54c3
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-02-21 16:28:37 -0700

    Added support for profiles.

--- js/ui.js
+++ js/ui.js
@@ -29,82 +29,86 @@ function settingsInit(id) {
         }
     }
 
-    s.page(          "settings-design",  "Place Objects");
-    s.file(          "fileSelect", {'binary': true, 'text': "Drop STL file here", 'callback': onFileChange});
-
+    s.page(             "settings-design",  "Place Objects");
+    s.file(                  "fileSelect", {'binary': true, 'text': "Drop STL file here", 'callback': onFileChange});
+                 
     s.separator(     "br");
-    s.button(          onAddToPlatform, "Add to Platform", {'id': "add_to_platform"});
-    s.button(          onClearPlatform, "Clear Platform",  {'id': "clear_platform"});
+    s.button(             onAddToPlatform, "Add Object",    {'id': "add_to_platform"});
+    s.button(             onClearPlatform, "Clear Objects", {'id': "clear_platform"});
+    
+    s.page(          "settings-profiles",  "Load Settings");
+    s.choice(       "machinePresetSelect", "Printer:")
+     .option( "lulzbot_mini2_aero_0.5mm" , "Lulzbot Mini 2 Aero 0.5 mm")
+     .option( "lulzbot_taz_we_aero_0.5mm", "Lulzbot TAZ Workhorse Aero 0.5 mm");
 
-    s.page(         "settings-machine", "Configure Machine");
-    s.heading(                          "Load Preset:");
-    s.choice(    "machinePresetSelect", "")
-     .option(           "lulzbot-mini", "Lulzbot Mini 2")
-     .option(         "deltaprintr-ks", "Deltaprintr Kickstarter Edition");
-    s.heading(                          "Machine:");
-    s.parameter(   "printerNozzleSize", "Nozzle (mm)",         0.4);
-    s.heading(                          "Build area:");
-    s.choice(    "platformStyleSelect", "Shape")
-     .option(            "rectangular", "Rectangular")
-     .option(            "circular",    "Circular");
-    s.parameter(     "printerMaxWidth", "Maximum width (mm)",  300);
-    s.parameter(     "printerMaxDepth", "Maximum depth (mm)",  300);
-    s.parameter(    "printerMaxHeight", "Maximum height (mm)", 300);
-    s.heading(                          "Start/End Gcode:");
-    s.choice(          "editGcodeMenu", "Edit gcode template")
-     .option(                   "none", "...")
-     .option(            "start-gcode", "start")
-     .option(              "end-gcode", "end");
-
-    s.page(              "start-gcode");
-    s.heading(                          "Start GCode template:");
-    s.textarea(           "startGcode");
-    s.button(         doneEditingGcode, "Done");
-
-    s.page(                "end-gcode");
-    s.heading(                          "End GCode template:");
-    s.textarea(             "endGcode");
-    s.button(         doneEditingGcode, "Done");
-
-    s.page(           "settings-print", "Slice and Print");
-    s.heading(                          "General:");
-    s.choice(    "slicingEngine",       "Slicing Engine:")
-     .option(                    "cura", "Cura")
-     .option(                  "slic3r", "Slic3r");
-    s.choice(    "materialPresetSelect", "Load Preset:")
-     .option(         "pla-high-speed", "PLA High Speed")
-     .option(        "pla-high-detail", "PLA High Detail");
-    s.heading(                          "Print Strength:");
-    s.choice(               "infill",   "Infill Pattern:")
-     .option(                   "grid", "Grid")
-     .option(                  "lines", "Lines")
-     .option(                 "gyroid", "Gyroid");
-    s.parameter(       "infillDensity", "Infill Density (%):",           30);
-    s.heading(                          "Speed and Temperature:");
-    s.parameter(    "printTemperature", "Printing temperature (C):",     200);
-    s.parameter(          "printSpeed", "Print speed (mm/s):",           50);
-    s.heading(                          "Filament:");
-    s.choice(     "filamentDiameter",   "Filament Diameter (mm):")
-     .option(                   "1.75", "1.75")
-     .option(                   "3.00", "3.00");
-    s.parameter(        "filamentFlow", "Flow (%):",                     100);
-    s.heading(                          "Scaffolding:");
-    s.choice(               "supports", "Support Material:")
-     .option(                   "none", "None")
-     .option(             "everywhere", "Everywhere")
-     .option(   "touching-build-plate", "Touching Build Plate");
-    s.choice(               "adhesion", "Bed Adhesion:")
-     .option(                  "skirt", "Skirt")
-     .option(                   "brim", "Brim")
-     .option(                   "raft", "Raft")
-     .option(                   "none", "None");
+    s.choice(      "materialPresetSelect", "Material:")
+     .option(    "polymaker_polylite_pla", "Polymaker Polylite PLA");
     s.separator();
-    s.button(           onSliceClicked, "Slice");
+    s.button(         onLoadPresetClicked, "Load");
+    s.buttonHelp("Loading new settings will<br>overwrite all modified values.");
+
+    s.page(            "settings-machine", "Machine Settings");
+    s.heading(                             "Machine:");
+    s.parameter(      "printerNozzleSize", "Nozzle Size (mm)",         0.4);
+    s.heading(                             "Build area:");
+    s.choice(       "platformStyleSelect", "Shape")
+     .option(               "rectangular", "Rectangular")
+     .option(               "circular",    "Circular");
+    s.parameter(        "printerMaxWidth", "Maximum width (mm)",  300);
+    s.parameter(        "printerMaxDepth", "Maximum depth (mm)",  300);
+    s.parameter(       "printerMaxHeight", "Maximum height (mm)", 300);
+    s.heading(                             "Start/End Gcode:");
+    s.choice(             "editGcodeMenu", "Edit gcode template")
+     .option(                      "none", "...")
+     .option(               "start-gcode", "start")
+     .option(                 "end-gcode", "end");
+
+    s.page(                 "start-gcode");
+    s.heading(                             "Start GCode template:");
+    s.textarea(              "startGcode");
+    s.button(            doneEditingGcode, "Done");
+                   
+    s.page(                   "end-gcode");
+    s.heading(                             "End GCode template:");
+    s.textarea(                "endGcode");
+    s.button(            doneEditingGcode, "Done");
+                   
+    s.page(              "settings-print", "Slice and Print");
+    s.heading(                             "Print Strength:");
+    s.choice(                  "infill",   "Infill Pattern:")
+     .option(                      "grid", "Grid")
+     .option(                     "lines", "Lines")
+     .option(                    "gyroid", "Gyroid");
+    s.parameter(          "infillDensity", "Infill Density (%):",           30);
+    s.parameter(           "wallLineCount","Wall Line Count:",           2);
+    s.heading(                             "Scaffolding:");
+    s.choice(                  "supports", "Support Material:")
+     .option(                      "none", "None")
+     .option(                "everywhere", "Everywhere")
+     .option(      "touching-build-plate", "Touching Build Plate");
+    s.parameter(          "infillDensity", "Support Angle:",           30);
+    s.choice(                  "adhesion", "Bed Adhesion:")
+     .option(                     "skirt", "Skirt")
+     .option(                      "brim", "Brim")
+     .option(                      "raft", "Raft")
+     .option(                      "none", "None");
+    s.heading(                             "Temperature and Speed:");
+    s.parameter(       "printTemperature", "Printing temperature (C):",     200);
+    s.parameter(         "bedTemperature", "Bed temperature (C):",     200);
+    s.parameter(             "printSpeed", "Print speed (mm/s):",           50);
+    s.parameter(            "layerHeight", "Layer Height (mm):",           0.25);
+    s.heading(                             "Filament:");
+    s.choice(          "filamentDiameter", "Filament Diameter (mm):")
+     .option(                      "1.75", "1.75")
+     .option(                      "3.00", "3.00");
+    s.parameter(           "filamentFlow", "Flow (%):",                     100);
+    s.separator();
+    s.button(              onSliceClicked, "Slice");
     s.buttonHelp("Click this button to save<br>gcode for your 3D printer.");
 
-    s.page(            "settings-help", "Help");
-    s.heading(                          "View Controls:");
-    s.element(                          "viewport-help");
+    s.page(               "settings-help", "Help");
+    s.heading(                             "View Controls:");
+    s.element(                             "viewport-help");
 
     s.done();
 
@@ -112,7 +116,7 @@ function settingsInit(id) {
 
     // Set the callbacks
 
-    document.getElementById("editGcodeMenu").onchange = onEditGcodeSelect;
+    document.getElementById("editGcodeMenu").onchange        = onEditGcodeSelect;
 
     onFileChange(); // Disable buttons
 }
@@ -122,6 +126,12 @@ function onEditGcodeSelect() {
     if(choice != "none") {
         settings.gotoPage(choice);
     }
+}
+
+function onLoadPresetClicked() {
+    slicer.loadDefaults();
+    slicer.loadProfile("machine", $("#machinePresetSelect").val() + ".toml");
+    slicer.loadProfile("print",   $("#materialPresetSelect").val() + ".toml");
 }
 
 function doneEditingGcode() {
@@ -149,9 +159,12 @@ function onClearPlatform() {
 }
 
 function onSliceClicked() {
-    showProgressBar();
-    stage.getAllGeometry().forEach((geo,i) => slicer.loadFromGeometry(geo, 'input' + (i || '') + '.stl'));
-    slicer.slice();
+    var geometries = stage.getAllGeometry();
+    if(geometries.length) {
+        showProgressBar();
+        stage.getAllGeometry().forEach((geo,i) => slicer.loadFromGeometry(geo, 'input' + (i || '') + '.stl'));
+        slicer.slice();
+    }
 }
 
 function showProgressBar() {
