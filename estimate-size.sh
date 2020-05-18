af6a3c361a401b5ea415f7017fa719a9df1eb9ea
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-05-18 12:42:51 -0600

    Added size estimation script.

---
+++ estimate-size.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+#
+# estimate-size.sh
+# Copyright (C) 2020 SynDaver Labs, Inc.
+#
+# This script builds the Cura engine from the source in BUILD_DIR
+# using Emscripten and copies the built files to INSTALL_DIR.
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+# Estimates the size of the entire slicer package if it were minimized and
+# stored in a zip file.
+
+getFiles() {
+    awk -F "'" '
+    /^const filesToCache/                   {PRINT=1}
+    /];/                                    {PRINT=0}
+                                            {sub(/\?.*$/, "", $2); if(PRINT) print $2}
+    ' service-worker.js | sed '/^\.$/ c index.html'
+}
+
+minify() {
+    rm -rf build
+    mkdir build
+    for file in $FILES
+    do
+        mkdir -p build/`dirname $file`
+        if [[ $file == *.js ]]; then
+            uglifyjs $file > build/$file
+        else
+            false
+        fi
+        if [ $? -ne 0 ]; then
+            cp $file build/$file
+        fi
+    done
+}
+
+FILES=`getFiles`
+
+minify $FILES
+
+zip -r -9 packed.zip build
+du -sh packed.zip
+
+echo
+
+echo Top ten largest files:
+find build -type f | xargs du -sh | sort -h | tail -10
