59b9dab5c21d223620bc34508bf27bb8e5a1f8da
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-05-26 09:14:00 -0600

    Allow slicing features to be toggled while preserving profile settings.

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ importScripts('lib/util/misc/Wikify.js');
 // Based on https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
 
 const info = {
-    version: '1.0.0',
+    version: '1.0.1',
     release: 1
 };
 
