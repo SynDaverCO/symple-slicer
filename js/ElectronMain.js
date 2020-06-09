e3f6a57235aaaa034e8727bcaf6496cc62424c97
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-06-09 08:37:05 -0600

    Initial commit.

---
+++ js/ElectronMain.js
@@ -0,0 +1,62 @@
+/**
+ * WebSlicer
+ * Copyright (C) 2020  SynDaver Labs, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Entry point for when using SympleSlicer as an Electron app
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    // Create the browser window.
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // Open the DevTools.
+    devtools = new BrowserWindow()
+    win.webContents.setDevToolsWebContents(devtools.webContents)
+    win.webContents.openDevTools({ mode: 'detach' })
+
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+}
+
+//app.commandLine.appendSwitch('--enable-gpu')
+//app.commandLine.appendSwitch('--enable-logging')
+
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
