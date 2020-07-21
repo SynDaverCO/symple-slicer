a4042230e8e545d02240219d8fed0ca7b6569a86
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-07-21 10:40:58 -0600

    Updated revision number.

--- src-app/service-worker.js
+++ src-app/service-worker.js
@@ -28,7 +28,7 @@ importScripts('lib/util/misc/Wikify.js');
 
 const info = {
     version: '1.0.7',
-    release: 9
+    release: 10
 };
 
 const cacheName = 'v' + info.version + "r" + info.release;
