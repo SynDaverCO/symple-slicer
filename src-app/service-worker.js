ac49733621799be6de07c1b261b9bc9e96a7cceb
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-08-19 11:09:06 -0600

    Fixed typo.

--- src-app/service-worker.js
+++ src-app/service-worker.js
@@ -27,7 +27,7 @@ importScripts('lib/util/misc/Wikify.js');
 // Based on https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
 
 const info = {
-    cacheVersion: 6
+    cacheVersion: 8
 };
 
 const cacheName = 'v' + info.cacheVersion;
