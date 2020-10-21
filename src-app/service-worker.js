2b2698e44022fd6999e9478d85970121bb01b140
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-10-21 14:20:59 -0600

    Update profiles.

--- src-app/service-worker.js
+++ src-app/service-worker.js
@@ -27,7 +27,7 @@ importScripts('lib/util/misc/Wikify.js');
 // Based on https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
 
 const info = {
-    cacheVersion: 53
+    cacheVersion: 58
 };
 
 const cacheName = 'v' + info.cacheVersion;
