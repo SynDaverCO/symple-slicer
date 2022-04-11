7b0933ec793102cac3031ce95daf3c3598efbb36
Marcio T <m.teixeira@syndaver.com>
2022-04-11 11:47:10 -0600

    Make buttons less obtrusive.

--- src-app/service-worker.js
+++ src-app/service-worker.js
@@ -27,7 +27,7 @@ importScripts('lib/util/misc/Wikify.js');
 // Based on https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
 
 const info = {
-    cacheVersion: 216
+    cacheVersion: 217
 };
 
 const cacheName = 'v' + info.cacheVersion;
