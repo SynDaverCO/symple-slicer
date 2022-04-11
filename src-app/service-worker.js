6e92daa35ecebe2b4b3d4648f18229c4fc3f3aea
Marcio T <m.teixeira@syndaver.com>
2022-04-11 12:07:32 -0600

    More UI adjustments.

--- src-app/service-worker.js
+++ src-app/service-worker.js
@@ -27,7 +27,7 @@ importScripts('lib/util/misc/Wikify.js');
 // Based on https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
 
 const info = {
-    cacheVersion: 217
+    cacheVersion: 218
 };
 
 const cacheName = 'v' + info.cacheVersion;
