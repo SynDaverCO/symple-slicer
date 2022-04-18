3dd2f081f51a525cce67f252ae9b766eb149fc2c
Marcio T <m.teixeira@syndaver.com>
2022-04-18 14:54:05 -0600

    Update version.

--- src-app/service-worker.js
+++ src-app/service-worker.js
@@ -27,7 +27,7 @@ importScripts('lib/util/misc/Wikify.js');
 // Based on https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
 
 const info = {
-    cacheVersion: 218
+    cacheVersion: 222
 };
 
 const cacheName = 'v' + info.cacheVersion;
