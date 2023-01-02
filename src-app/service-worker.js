34a8d2f6048c032c3683f9830eee15d7945cc870
Marcio T <m.teixeira@syndaver.com>
2023-01-02 13:19:48 -0700

    Fixed service worker.

--- src-app/service-worker.js
+++ src-app/service-worker.js
@@ -22,7 +22,7 @@
  *
  */
 
-importScripts('lib/util/misc/Wikify.js');
+import { wikify } from './lib/util/misc//Wikify.js';
 
 // Based on https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
 
